4181558fd3c22459d41be1ed0d57047997a54895
Nathan Stewart <stewart.nathang@gmail.com>
2017-09-29 09:48:26 -0700

    add header with title and basic blurb

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./Header";
 
 function App(props) {
   return (
     <div>
-      <h1>Working</h1>
+      <Header/>
+      <hr/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header(){
+  return (
+    <div>
+      <div>
+        <h1>Portfolio</h1>
+        <p>A collection of work by Nathan Stewart</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
